7bae66362f4e586741cf33611db658fe7d20b273
Matthew Wear <matt_wear@volusion.com>
2014-07-28 15:08:38 -0500

    formatting of order list

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/adapters/OrdersAdapter.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/adapters/OrdersAdapter.java
@@ -1,6 +1,7 @@
 package com.mozu.mozuandroidinstoreassistant.app.adapters;
 
 import android.content.Context;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,19 @@ import android.widget.TextView;
 import com.mozu.api.contracts.commerceruntime.orders.Order;
 import com.mozu.mozuandroidinstoreassistant.app.R;
 
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class OrdersAdapter extends ArrayAdapter<Order> {
+
+    private NumberFormat mNumberFormat;
 
     public OrdersAdapter(Context context) {
         super(context, R.layout.order_list_item);
+
+        mNumberFormat = NumberFormat.getCurrencyInstance();
     }
 
     @Override
@@ -31,10 +41,15 @@ public class OrdersAdapter extends ArrayAdapter<Order> {
         TextView total = (TextView) convertView.findViewById(R.id.order_total);
 
         orderNo.setText(String.valueOf(order.getOrderNumber()));
-        orderDate.setText(order.getSubmittedDate() != null ? order.getSubmittedDate().toString() : "");
+
+        android.text.format.DateFormat dateFormat= new android.text.format.DateFormat();
+        String date = order.getSubmittedDate() != null ? dateFormat.format("MM/dd/yy  hh:mm a", new Date(order.getSubmittedDate().getMillis())).toString() : "";
+
+        orderDate.setText(date);
         email.setText(order.getEmail());
         status.setText(order.getStatus());
-        total.setText(String.valueOf(order.getTotal()));
+
+        total.setText(mNumberFormat.format(order.getTotal()));
 
         return convertView;
     }
