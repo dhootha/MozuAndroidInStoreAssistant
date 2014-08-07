8bf18695e594031750466a7fefc4a787585cc513
Matthew Wear <matt_wear@volusion.com>
2014-08-07 13:40:26 -0500

    fixed view pager height issue, well, made it better, still could use improvement

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailFullfillmentFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailFullfillmentFragment.java
@@ -5,14 +5,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.mozu.api.contracts.commerceruntime.fulfillment.*;
+import com.mozu.api.contracts.commerceruntime.fulfillment.Package;
 import com.mozu.api.contracts.commerceruntime.orders.Order;
+import com.mozu.api.contracts.commerceruntime.orders.OrderItem;
 import com.mozu.mozuandroidinstoreassistant.app.R;
 
 
 public class OrderDetailFullfillmentFragment extends Fragment {
 
+    public static final String PENDING = "Pending";
+    public static final String FULFILLED = "Fulfilled";
     private Order mOrder;
+
+    private TextView mPendingTotal;
+    private TextView mFulfilledTotal;
+    private TextView mShipmentTotal;
 
     public OrderDetailFullfillmentFragment() {
         // Required empty public constructor
@@ -24,6 +34,10 @@ public class OrderDetailFullfillmentFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.order_detail_fulfillment_fragment, null);
 
+        mPendingTotal = (TextView) view.findViewById(R.id.shipment_pending_total);
+        mFulfilledTotal = (TextView) view.findViewById(R.id.shipment_fulfilled_total);
+        mShipmentTotal = (TextView) view.findViewById(R.id.shipment_total);
+
         if (mOrder != null) {
             setOrderToViews(view);
         }
@@ -32,6 +46,27 @@ public class OrderDetailFullfillmentFragment extends Fragment {
     }
 
     private void setOrderToViews(View view) {
+
+        int pendingCount = 0;
+        int fulfilledCount = 0;
+
+        for (Package orderPackage: mOrder.getPackages()) {
+
+            String status = orderPackage.getStatus();
+
+            if (status.equalsIgnoreCase(PENDING)) {
+                pendingCount++;
+            }
+
+            if (status.equalsIgnoreCase(FULFILLED)) {
+                fulfilledCount++;
+            }
+
+        }
+
+        mPendingTotal.setText(String.valueOf(pendingCount));
+        mFulfilledTotal.setText(String.valueOf(fulfilledCount));
+        mShipmentTotal.setText(String.valueOf(pendingCount + fulfilledCount));
 
     }
 
