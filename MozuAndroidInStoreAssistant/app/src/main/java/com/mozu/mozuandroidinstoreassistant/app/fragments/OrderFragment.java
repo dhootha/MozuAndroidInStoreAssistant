c0c6de020995f8921398b3a1483d47f2412446d3
Santhosh Mankala <santhosh_mankala@volusion.com>
2014-08-21 14:04:45 -0500

    Merge branch 'master' into B-27699
    Conflicts:
    	MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderFragment.java

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderFragment.java
@@ -39,8 +39,8 @@ public class OrderFragment extends Fragment implements LoaderManager.LoaderCallb
 
     private static final int LOADER_ORDERS = 523;
 
-    private int mTenantId;
-    private int mSiteId;
+    private Integer mTenantId;
+    private Integer mSiteId;
 
     private ListView mOrdersList;
     private LinearLayout mProgress;
@@ -188,11 +188,11 @@ public class OrderFragment extends Fragment implements LoaderManager.LoaderCallb
         userState.updateUserPreferences();
     }
 
-    public void setTenantId(int tenantId) {
+    public void setTenantId(Integer tenantId) {
         mTenantId = tenantId;
     }
 
-    public void setSiteId(int siteId) {
+    public void setSiteId(Integer siteId) {
         mSiteId = siteId;
     }
 
