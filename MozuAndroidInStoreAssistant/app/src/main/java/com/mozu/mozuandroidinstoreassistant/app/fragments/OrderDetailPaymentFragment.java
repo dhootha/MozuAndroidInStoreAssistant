b4d5f6a9dde4cbc982403e66dcb0ff7d572980a3
Matthew Wear <matt_wear@volusion.com>
2014-08-22 14:25:49 -0500

    shored up payments tab

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailPaymentFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailPaymentFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,11 +21,15 @@ public class OrderDetailPaymentFragment extends Fragment {
 
     private Order mOrder;
 
+    private NumberFormat mNumberFormat;
+
     public OrderDetailPaymentFragment() {
+
         // Required empty public constructor
         setRetainInstance(true);
-    }
 
+        mNumberFormat = NumberFormat.getCurrencyInstance();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,25 +43,28 @@ public class OrderDetailPaymentFragment extends Fragment {
     }
 
     private void setOrderToViews(View view) {
-
         ListView paymentList = (ListView) view.findViewById(R.id.payments_list);
         paymentList.setAdapter(new OrderDetailPaymentsAdapter(getActivity(), mOrder.getPayments()));
+        TextView emptyView = (TextView) view.findViewById(R.id.empty_payments_message);
+        paymentList.setEmptyView(emptyView);
+
+        if (mOrder.getPayments() == null || mOrder.getPayments().size() < 1) {
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            emptyView.setVisibility(View.GONE);
+        }
 
         TextView orderTotal = (TextView) view.findViewById(R.id.order_total);
         TextView paymentsReceived = (TextView) view.findViewById(R.id.payments_received);
         TextView balance = (TextView) view.findViewById(R.id.balance);
 
-        NumberFormat format = NumberFormat.getInstance();
-
-        orderTotal.setText(format.format(mOrder.getTotal()));
-        paymentsReceived.setText(format.format(mOrder.getTotal() - mOrder.getAmountRemainingForPayment()));
-        balance.setText(format.format(mOrder.getAmountRemainingForPayment()));
+        orderTotal.setText(mNumberFormat.format(mOrder.getTotal()));
+        paymentsReceived.setText(mNumberFormat.format(mOrder.getTotal() - mOrder.getAmountRemainingForPayment()));
+        balance.setText(mNumberFormat.format(mOrder.getAmountRemainingForPayment()));
     }
-
 
     public void setOrder(Order order) {
         mOrder = order;
     }
-
 
 }
