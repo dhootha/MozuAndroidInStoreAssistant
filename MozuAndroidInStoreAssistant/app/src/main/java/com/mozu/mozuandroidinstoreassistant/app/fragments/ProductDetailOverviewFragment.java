46520eab62472d9c3dddfb6cc63b17926f8cf986
Santhosh Mankala <santhosh_mankala@volusion.com>
2014-09-05 15:12:46 -0400

    Fix Image upload task

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/ProductDetailOverviewFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/ProductDetailOverviewFragment.java
@@ -36,6 +36,7 @@ public class ProductDetailOverviewFragment extends Fragment {
 
     private static final int MAX_DESC_LENGTH = 500;
     private static final String PRODUCT_CONFIGURABLE = "Configurable";
+    private static final String PRODUCT_BUNDLE = "Bundle";
 
     public ProductDetailOverviewFragment() {
         // Required empty public constructor
@@ -108,6 +109,11 @@ public class ProductDetailOverviewFragment extends Fragment {
 
     private boolean isProductConfigurable(Product product){
         return PRODUCT_CONFIGURABLE.equalsIgnoreCase(product.getProductUsage());
+    }
+
+    private boolean isProductBundled(Product product){
+        return PRODUCT_BUNDLE.equalsIgnoreCase(product.getProductUsage());
+
     }
 
     private String getUPC(Product product){
