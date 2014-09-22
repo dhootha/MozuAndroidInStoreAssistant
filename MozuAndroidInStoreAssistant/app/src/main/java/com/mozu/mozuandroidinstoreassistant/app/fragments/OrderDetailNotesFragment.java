807b7aefc89ddde2c8a41296aa490602b7e0c4ec
Santhosh Mankala <santhosh_mankala@volusion.com>
2014-09-22 16:08:52 -0400

    Notes tab fix

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailNotesFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/fragments/OrderDetailNotesFragment.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import com.mozu.api.contracts.commerceruntime.orders.Order;
 import com.mozu.mozuandroidinstoreassistant.app.R;
 import com.mozu.mozuandroidinstoreassistant.app.adapters.OrderDetailNotesAdapter;
+import com.mozu.mozuandroidinstoreassistant.app.views.LoadingView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,8 +22,16 @@ public class OrderDetailNotesFragment extends Fragment {
 
     private Order mOrder;
 
-    @InjectView(R.id.notes_list_layout) LinearLayout mNoteListLayout;
-    @InjectView(R.id.empty_notes_message) TextView mEmptyText;
+    @InjectView(R.id.internal_notes_layout) LinearLayout mNoteListLayout;
+    @InjectView(R.id.customer_notes_layout) LinearLayout mCustomerNotesListLayout;
+    @InjectView(R.id.show_customer_notes) TextView mShowCustomerNotes;
+    @InjectView(R.id.show_internal_notes) TextView mShowInternalNotes;
+
+private LoadingView mNotesLoadingView;
+    private LoadingView mCustomerLoadingView;
+
+
+
 
     public OrderDetailNotesFragment() {
         // Required empty public constructor
@@ -34,6 +43,8 @@ public class OrderDetailNotesFragment extends Fragment {
         View view = inflater.inflate(R.layout.order_detail_notes_fragment, null);
 
         ButterKnife.inject(this, view);
+         mNotesLoadingView = (LoadingView) view.findViewById(R.id.notes_loading_view);
+         mCustomerLoadingView = (LoadingView) view.findViewById(R.id.customer_notes_loading_view);
 
         if (mOrder != null) {
             setOrderToViews(view);
@@ -44,15 +55,36 @@ public class OrderDetailNotesFragment extends Fragment {
 
     private void setOrderToViews(View view) {
 
-        if (mOrder.getNotes() == null || mOrder.getNotes().size() < 1) {
-            mNoteListLayout.setVisibility(View.GONE);
-            mEmptyText.setVisibility(View.VISIBLE);
-
-            return;
+        ListView noteList = (ListView) view.findViewById(R.id.notes_list);
+        noteList.setAdapter(new OrderDetailNotesAdapter(mOrder, true));
+        ListView customerNotesList = (ListView) view.findViewById(R.id.customer_list);
+        customerNotesList.setAdapter(new OrderDetailNotesAdapter(mOrder, false));
+        if (mOrder == null || mOrder.getNotes().size() < 1) {
+            mNotesLoadingView.setError("No Notes Available");
+        } else {
+            mNotesLoadingView.success();
         }
 
-        ListView noteList = (ListView) view.findViewById(R.id.notes_list);
-        noteList.setAdapter(new OrderDetailNotesAdapter(getActivity(), mOrder.getNotes()));
+        if (mOrder == null || mOrder.getShopperNotes() == null) {
+            mCustomerLoadingView.setError("No Notes Available");
+        } else {
+            mCustomerLoadingView.success();
+        }
+
+        showInternalNotes();
+        mShowCustomerNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomerNotes();
+            }
+        });
+
+        mShowInternalNotes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                showInternalNotes();
+            }
+        });
 
     }
 
@@ -60,5 +92,13 @@ public class OrderDetailNotesFragment extends Fragment {
         mOrder = order;
     }
 
+    public void showCustomerNotes(){
+        mCustomerNotesListLayout.setVisibility(View.VISIBLE);
+        mNoteListLayout.setVisibility(View.GONE);
+    }
 
+    public void showInternalNotes(){
+        mCustomerNotesListLayout.setVisibility(View.GONE);
+        mNoteListLayout.setVisibility(View.VISIBLE);
+    }
 }
