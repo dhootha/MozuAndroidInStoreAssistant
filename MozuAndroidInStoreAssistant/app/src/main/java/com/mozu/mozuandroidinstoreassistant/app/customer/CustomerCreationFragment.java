711fe31865af108aabd97cd64cff89f718d6683f
chris_pound <chris_pound@volusion.com>
2015-08-11 14:55:40 -0500

    switched to rxjava can validate address verifying create customer form.

--- MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/customer/CustomerCreationFragment.java
+++ MozuAndroidInStoreAssistant/app/src/main/java/com/mozu/mozuandroidinstoreassistant/app/customer/CustomerCreationFragment.java
@@ -2,30 +2,35 @@ package com.mozu.mozuandroidinstoreassistant.app.customer;
 
 import android.app.AlertDialog;
 import android.app.Fragment;
-import android.app.LoaderManager;
 import android.content.DialogInterface;
-import android.content.Loader;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.mozu.api.contracts.core.Address;
 import com.mozu.api.contracts.customer.AddressValidationResponse;
 import com.mozu.mozuandroidinstoreassistant.app.OrderCreationActivity;
 import com.mozu.mozuandroidinstoreassistant.app.R;
-import com.mozu.mozuandroidinstoreassistant.app.customer.loaders.CustomerAddressValidationLoader;
+import com.mozu.mozuandroidinstoreassistant.app.customer.loaders.CustomerAddressValidation;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import rx.Observable;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by chris_pound on 8/7/15.
  */
-public class CustomerCreationFragment extends Fragment implements LoaderManager.LoaderCallbacks<AddressValidationResponse>, CustomerAddressVerifier {
+public class CustomerCreationFragment extends Fragment implements CustomerAddressVerifier {
 
     private static final int CUSTOMER_ADDRESS_VALIDATION_LOADER = 510;
     @InjectView(R.id.first_name)
@@ -34,8 +39,8 @@ public class CustomerCreationFragment extends Fragment implements LoaderManager.
     EditText mLastName;
     @InjectView(R.id.phone_number)
     EditText mPhoneNumbeer;
-    @InjectView(R.id.phone_type)
-    EditText mPhoneType;
+    @InjectView(R.id.address_type)
+    Spinner mAddressType;
     @InjectView(R.id.email)
     EditText mEmail;
     @InjectView(R.id.address_1)
@@ -45,7 +50,7 @@ public class CustomerCreationFragment extends Fragment implements LoaderManager.
     @InjectView(R.id.city)
     EditText mCity;
     @InjectView(R.id.state)
-    EditText mState;
+    Spinner mState;
     @InjectView(R.id.country)
     EditText mCountry;
     @InjectView(R.id.zipcode)
@@ -58,7 +63,9 @@ public class CustomerCreationFragment extends Fragment implements LoaderManager.
     Button mVerify;
     private int mTenantId;
     private int mSiteId;
-    private CustomerAddressValidationLoader mCustomerAddressValidationLoader;
+    private String mAddressTypeSelected;
+    private String mStateSelected;
+    private Observable<AddressValidationResponse> addressValidationResponseObservable;
 
     public static CustomerCreationFragment getInstance(int tenantId, int siteId) {
         Bundle bundle = new Bundle();
@@ -74,7 +81,6 @@ public class CustomerCreationFragment extends Fragment implements LoaderManager.
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_create_customer, container, false);
         ButterKnife.inject(this, view);
-        getLoaderManager().initLoader(CUSTOMER_ADDRESS_VALIDATION_LOADER, null, this);
         mTenantId = getArguments().getInt(OrderCreationActivity.CURRENT_TENANT_ID);
         mSiteId = getArguments().getInt(OrderCreationActivity.CURRENT_SITE_ID);
         return view;
@@ -83,76 +89,172 @@ public class CustomerCreationFragment extends Fragment implements LoaderManager.
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.address_type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mAddressType.setAdapter(adapter);
+        mAddressType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mAddressTypeSelected = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        mState.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mStateSelected = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        ArrayAdapter<CharSequence> stateAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.states, android.R.layout.simple_spinner_item);
+        stateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mState.setAdapter(stateAdapter);
         mVerify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Address addressToVerify = new Address();
                 addressToVerify.setAddress1(mAddress1.getText().toString());
                 addressToVerify.setAddress2(mAddress2.getText().toString());
-                addressToVerify.setStateOrProvince(mState.getText().toString());
+                addressToVerify.setStateOrProvince(mStateSelected);
                 addressToVerify.setCityOrTown(mCity.getText().toString());
                 addressToVerify.setPostalOrZipCode(mZip.getText().toString());
                 addressToVerify.setCountryCode(mCountry.getText().toString());
+                addressToVerify.setAddressType(mAddressTypeSelected);
                 verifyAddressIsValid(addressToVerify);
             }
         });
-
     }
 
-    @Override
-    public Loader<AddressValidationResponse> onCreateLoader(int id, Bundle args) {
-        mCustomerAddressValidationLoader = new CustomerAddressValidationLoader(getActivity().getApplicationContext(), mTenantId, mSiteId);
-        return mCustomerAddressValidationLoader;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<AddressValidationResponse> loader, AddressValidationResponse data) {
-        if (data.getAddressCandidates().size() > 0 && data != null) {
-            Address address = data.getAddressCandidates().get(0);
-            final AlertDialog alert = new AlertDialog.Builder(getActivity().getApplicationContext())
-                    .setTitle("Validated Address")
-                    .setMessage(address.getAddress1() + "\n" + address.getCityOrTown() + ", " + address.getStateOrProvince() + " " + address.getPostalOrZipCode() + "\n" + address.getCountryCode())
-                    .setPositiveButton("Use This", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                        }
-                    })
-                    .setNegativeButton("Keep Original", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            if (dialog != null)
-                                dialog.dismiss();
-
-                        }
-                    })
-                    .create();
-            alert.show();
-        } else {
-            final AlertDialog alert = new AlertDialog.Builder(getActivity().getApplicationContext())
-                    .setTitle("Validated Address")
-                    .setMessage("Address is Valid.")
-                    .create();
-            alert.show();
-
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<AddressValidationResponse> loader) {
-
-    }
-
-    @Override
     public void verifyAddressIsValid(Address address) {
-        getCustomerAddressValidationLoader().setAddressToVerify(address);
-        getCustomerAddressValidationLoader().forceLoad();
+        if(!validateForm()) {
+            return;
+        }
+        addressValidationResponseObservable = new CustomerAddressValidation(mTenantId, mSiteId).getAddressValidationObservable(address);
+        addressValidationResponseObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(createAddressValidationSubscriber());
+
     }
 
-    private CustomerAddressValidationLoader getCustomerAddressValidationLoader() {
-        if (mCustomerAddressValidationLoader == null) {
-            Loader loader = getLoaderManager().getLoader(CUSTOMER_ADDRESS_VALIDATION_LOADER);
-            mCustomerAddressValidationLoader = (CustomerAddressValidationLoader) loader;
-        }
-        return mCustomerAddressValidationLoader;
+    private Subscriber<AddressValidationResponse> createAddressValidationSubscriber() {
+        return new Subscriber<AddressValidationResponse>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                AlertDialog error = new AlertDialog.Builder(getActivity())
+                        .setMessage(e.toString())
+                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                error.show();
+            }
+
+            @Override
+            public void onNext(AddressValidationResponse response) {
+                if (response != null && response.getAddressCandidates() != null && response.getAddressCandidates().size() > 0) {
+                    Address address = response.getAddressCandidates().get(0);
+                    createSuggestedAddressDialog(address).show();
+                } else {
+                    createValidatedAddressDialog().show();
+                }
+            }
+        };
     }
+
+    private AlertDialog createValidatedAddressDialog() {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.verify_title)
+                .setMessage(R.string.valid_address)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+    }
+
+    private AlertDialog createSuggestedAddressDialog(final Address address) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.verify_title)
+                .setMessage(address.getAddress1() + "\n" +
+                        address.getCityOrTown() + ", " +
+                        address.getStateOrProvince() + " " +
+                        address.getPostalOrZipCode() + "\n" +
+                        address.getCountryCode())
+                .setPositiveButton(R.string.verify_use_this, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mAddress1.setText(address.getAddress1());
+                        mAddress2.setText(address.getAddress2());
+                        mCity.setText(address.getCityOrTown());
+//                        mState.setText(address.getStateOrProvince());
+                        mZip.setText(address.getPostalOrZipCode());
+                        mCountry.setText(address.getCountryCode());
+                    }
+                })
+                .setNegativeButton(R.string.verify_keep, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (dialog != null)
+                            dialog.dismiss();
+                    }
+                })
+                .create();
+    }
+    
+    private boolean validateForm() {
+        boolean isValid = true;
+        if(mFirstName.getText().toString().isEmpty()) {
+            mFirstName.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mLastName.getText().toString().isEmpty()) {
+            mLastName.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mPhoneNumbeer.getText().toString().isEmpty()) {
+            mPhoneNumbeer.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mEmail.getText().toString().isEmpty()) {
+            mEmail.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mAddress1.getText().toString().isEmpty()) {
+            mAddress1.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mCity.getText().toString().isEmpty()) {
+            mCity.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mZip.getText().toString().isEmpty()) {
+            mZip.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        if(mCountry.getText().toString().isEmpty()) {
+            mCountry.setError(getActivity().getResources().getString(R.string.required));
+            isValid = false;
+        }
+        return isValid;
+    }
+
 }
